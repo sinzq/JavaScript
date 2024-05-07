c3bd23941fd2428edd7f9fb33373a29fd251e7c6
sinzq <shivam.singh8@yahoo.com>
2024-05-07 16:54:35 +0530

    this commit on 07MAY2024_1700

---
+++ DataType_convension.js
@@ -0,0 +1,24 @@
+console.log("DataTypes_conversion")
+
+let age = "33"
+
+console.log(typeof age)
+console.log(typeof (age))
+
+let value_number = String(age)
+console.log(typeof value_number)
+
+
+// conversion history 
+// "33" -- 33
+// "3a" -- NaN
+// true -- 1 > false --0
+
+isloggedin = 0
+let value = Boolean(isloggedin)
+console.log(value)
+
+let some = 33
+let som = String(some)
+console.log(typeof som)
+console.log(som)
