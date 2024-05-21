491f9dd185ae2a3e7071e49ae22008f6481edb91
Shivam Singh <122508336+sinzq@users.noreply.github.com>
2024-05-21 10:00:24 +0000

    converstion

--- Day_01_ basic/03_DatatypeConversion.js
+++ Day_01_ basic/03_DatatypeConversion.js
@@ -1,10 +1,74 @@
-let score = "33ab"
+let score = 33
 
 console.log(typeof score)                  // to check datatype
 
-let val = Number(score)                    // to convert string to number
-// let val = String(score)                 // to convert number to string
+let val = String(score)
 
-console.log(typeof val)                    // to check datatype
-console.log(score)
+console.log(typeof val)
 
+
+
+let scores = 33
+
+let valt = Number(scores)                   // to convert number to string 
+
+console.log(typeof valt)                    // to check datatype
+
+
+
+let scoress = "33s"
+
+let valtt = Number(scoress)                 // to convert string to number
+
+// let val = String(score)                  // to convert number to string
+
+console.log(typeof valtt)                   // to check datatype
+
+console.log(valtt)                          // showing NaN
+
+
+
+// NUll datatype converstion
+let scoresss = null
+
+let valttt = Number(scoresss)                 // to convert string to number
+
+console.log(typeof valttt)                   // to check datatype
+
+console.log(valttt)                          // showing NaN
+
+
+
+
+// undefined datatype converstion
+let scoressss = undefined
+
+let valtttt = Number(scoressss)                // to convert string to number
+
+console.log(typeof valtttt)                   // to check datatype
+
+console.log(valtttt)                          // showing NaN
+
+
+
+// boolean values datatype converstion
+let scoresssss = true
+
+let valttttt = Number(scoresssss)               // to convert string to number
+
+console.log(typeof valttttt)                   // to check datatype
+
+console.log(valttttt)                          // showing NaN
+
+
+// "33"    => 33
+// "33abc" => NaN
+// true    => 1
+// false   => 0
+
+
+// Boolean converstion
+// 1       => true
+// 0       => false
+// "String"=> true
+// ""=> false
