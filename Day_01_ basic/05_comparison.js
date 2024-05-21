e4135c89689497ec496c56c828342fcf6ebd7511
Shivam Singh <122508336+sinzq@users.noreply.github.com>
2024-05-21 10:33:42 +0000

    comarision

---
+++ Day_01_ basic/05_comparison.js
@@ -0,0 +1,20 @@
+let a = 1
+let b = 2
+
+// console.log(a < b)                   // > -- Greater Than   ex -- 10 > 8
+// console.log(a > b)
+// console.log(a == b)                  // < -- Less Than      ex -- 8 < 10
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a = b)
+
+console.table([a>b, a<b, a=b, a!=b, a<=b, a>=b])
+
+console.log(null < 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log("NXT LINE")
+// === 
+console.log("2" === 2)
+console.log("2" == 2)
