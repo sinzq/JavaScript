c9ef495f55caa4ddc89929d0565524648d3d70e6
Shivam Singh <122508336+sinzq@users.noreply.github.com>
2024-05-21 10:53:52 +0000

    ques

---
+++ Interview_question/Ques.js
@@ -0,0 +1,21 @@
+// primitive data types 7 types
+// String  -- "string"
+// Number  -- 12 / 12.5 
+// Boolean -- true / false
+// Null    -- null
+// undefined
+// symbol -- used for unique values in react js 
+// BigInt  -- used to scientific values
+
+
+//  non-primitive and reference data types.
+// array    =  ["shaktiman" "hatim"]
+// object   = {key : value, age : 22}  pair of key values 
+// function =  function(){}
+
+let f =  function(){
+    console.log("Hello GitHub")
+}
+
+f();
+
