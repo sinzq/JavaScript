b9f7220690949b8a76f96ec710b04d61f6cd12cd
Shivam Singh <122508336+sinzq@users.noreply.github.com>
2024-05-21 09:19:54 +0000

    dataconversion

---
+++ Day_01_ basic/03_DatatypeConversion.js
@@ -0,0 +1,10 @@
+let score = "33ab"
+
+console.log(typeof score)                  // to check datatype
+
+let val = Number(score)                    // to convert string to number
+// let val = String(score)                 // to convert number to string
+
+console.log(typeof val)                    // to check datatype
+console.log(score)
+
