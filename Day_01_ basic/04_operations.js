268136ac37c622e053373acaa2095eb28aa261db
Shivam Singh <122508336+sinzq@users.noreply.github.com>
2024-05-21 10:17:15 +0000

    operations

---
+++ Day_01_ basic/04_operations.js
@@ -0,0 +1,30 @@
+var a = 1
+var b = 2
+
+// console.log(b)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)
+
+
+console.table([a+b, a-b, a*b, a**b, a%b, a/b])
+console.log(typeof b)
+
+
+let str1 ="hello"
+let str2 =" Sinzq"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log(1 + str1)
+console.log(1 + "1")
+console.log(1 + "1" + 1)
+console.log(1 + 1 + "1")
+console.log("1" + 1 + 1)
+console.log((1 + 1) + 1)
+
+console.log(+true)
